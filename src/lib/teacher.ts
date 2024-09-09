cf201637b29d1f91b1130a28d2652155add9b6d7
KareemRoshdy <karem22roshdy@gmail.com>
2024-09-09 21:21:06 +0300

    Add New Admin ID

--- src/lib/teacher.ts
+++ src/lib/teacher.ts
@@ -1,3 +1,6 @@
 export const isTeacher = (userId?: string | null) => {
-  return userId === process.env.NEXT_PUBLIC_ADMIN_ID;
+  return (
+    userId === process.env.NEXT_PUBLIC_ADMIN_ID ||
+    userId === "user_2ki7DyXsSCkij9Zk3PaSsQiuZ1q"
+  );
 };
