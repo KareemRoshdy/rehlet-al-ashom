01816349e0951c039e1e320ae0ded79627665987
KareemRoshdy <karem22roshdy@gmail.com>
2024-11-07 21:54:20 +0200

    Update Admin ID

--- src/lib/teacher.ts
+++ src/lib/teacher.ts
@@ -1,6 +1,7 @@
 export const isTeacher = (userId?: string | null) => {
   return (
     userId === process.env.NEXT_PUBLIC_ADMIN_ID ||
-    userId === "user_2ki7DyXsSCkij9Zk3PaSsQiuZ1q"
+    userId === "user_2ki7DyXsSCkij9Zk3PaSsQiuZ1q" ||
+    userId === "user_2kbXM1HAgwflWyOlbvRqN7AXdOv" 
   );
 };
